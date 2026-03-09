faaf115ac49df032581e710b23d9d7690aa79ccc
Weston Steimel <author@code.w.steimel.me.uk>
2026-03-09 09:09:17 +0000

    disable wordfence id mapper (#53)
    Signed-off-by: Weston Steimel <author@code.w.steimel.me.uk>

--- src/anchore_security_cli/identifiers/providers/wordfence.py
+++ src/anchore_security_cli/identifiers/providers/wordfence.py
@@ -1,3 +1,4 @@
+from rich.pretty import d
 import logging
 
 import requests
@@ -13,24 +14,26 @@ class Wordfence(Provider):
             name="Wordfence",
         )
 
+    # def _fetch(self) -> list[ProviderRecord]:
+    #     records = []
+    #     logging.debug(f"Start downloading latest {self.name} content")
+    #     result = requests.get(self.url, timeout=10).json()
+    #     logging.debug(f"Finish downloading latest {self.name} content")
+    #     logging.debug(f"Start processing {self.name} alias records")
+    #     for r in result.values():
+    #         cve = r.get("cve")
+    #         published = r.get("published")
+
+    #         if cve and published:
+    #             records.append(
+    #                 ProviderRecord(
+    #                     id=cve,
+    #                     aliases=Aliases.from_list([cve, cve_to_gcve(cve)]),
+    #                     published=self._parse_date(published),
+    #                 ),
+    #             )
+    #     logging.debug(f"Finish processing {self.name} alias records")
+    #     return records
+
     def _fetch(self) -> list[ProviderRecord]:
-        records = []
-        logging.debug(f"Start downloading latest {self.name} content")
-        result = requests.get(self.url, timeout=10).json()
-        logging.debug(f"Finish downloading latest {self.name} content")
-        logging.debug(f"Start processing {self.name} alias records")
-        for r in result.values():
-            cve = r.get("cve")
-            published = r.get("published")
-
-            if cve and published:
-                records.append(
-                    ProviderRecord(
-                        id=cve,
-                        aliases=Aliases.from_list([cve, cve_to_gcve(cve)]),
-                        published=self._parse_date(published),
-                    ),
-                )
-        logging.debug(f"Finish processing {self.name} alias records")
-
-        return records
+        return []
