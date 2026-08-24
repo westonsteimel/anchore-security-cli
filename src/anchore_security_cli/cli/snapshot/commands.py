48d41a1bebb273838542ea8ea45936c771ad61e8
Weston Steimel <author@code.w.steimel.me.uk>
2026-08-24 12:41:40 +0100

    fix: remove commit and push params as well
    Signed-off-by: Weston Steimel <author@code.w.steimel.me.uk>

--- src/anchore_security_cli/cli/snapshot/commands.py
+++ src/anchore_security_cli/cli/snapshot/commands.py
@@ -13,5 +13,5 @@ def group(_: Application):
 @group.command(name="cve5", help="Allocate Anchore security identifiers")
 @click.option("--repo-root", help="Path to the root of the existing CVE5 dataset git repo", required=True)
 @click.pass_obj
-def cve5_snapshot(cfg: Application, repo_root: str, commit: bool, push: bool) -> None:
+def cve5_snapshot(cfg: Application, repo_root: str) -> None:
     CVE5Snapshotter(repo_root).process()
